389c109b99c2346cf127733ab70a55d7ec65135c
jobzhao <postmaster@huo15.com>
2026-04-05 23:11:28 +0800

    fix: 从 .gitignore 移除 hooks/

---
+++ hooks/init.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const channel_1 = require("../dist/channel");
+exports.default = async function (opts) {
+    if (opts.registerChannel) {
+        opts.registerChannel({
+            id: "huo15-dingtalk-connector-pro",
+            name: "DingTalk Pro",
+            description: "火一五定制版钉钉连接器",
+            logoUrl: "https://cnstatic01.e.aliyuncs.com/rta/assets/favicon.ico",
+            factory: channel_1.DingTalkChannel,
+            envVars: {
+                DINGTALK_CLIENT_ID: { description: "DingTalk AppKey" },
+                DINGTALK_CLIENT_SECRET: { description: "DingTalk AppSecret" }
+            }
+        });
+    }
+};
